bfc1eeb2c414f07ddade7ea2a6c5a5667ca606b1
rqueztech <rqueztech@noreply.com>
2024-07-08 11:16:40 +0900

    Set up initial calculator.

---
+++ subnetcalculator.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+)
+
+func checkclassc(ipinput) string {
+    
+}
+
+func main() {
+    var ipaddress string
+    fmt.Print("Enter Network: ")
+    fmt.Scanf("%s", &ipaddress)
+
+}
